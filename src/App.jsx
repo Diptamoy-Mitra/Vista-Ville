c00467b0a72490bd619d98c52d53b9609084d839
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-09-28 17:26:01 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="App">
+      <h1 className='text-2xl'>Hello World!</h1>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
