013fc5d45ccb872285064739a9c13d289d269e8b
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-10-03 14:56:06 +0530

    sign in and navigate home

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,25 +1,41 @@
 import React, { useState } from 'react'
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { toast } from 'react-toastify';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 const Signin = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
-
   const { email, password } = formData;
-
-  const [showPassword, setShowPassword] = useState(false);
-
-  const onChange = (e) => {
-    // console.log(e.target.value)
+  const navigate = useNavigate();
+  function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
-      [e.target.id]: e.target.value
-    }))
+      [e.target.id]: e.target.value,
+    }));
   }
-
+ 
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Bad user credentials");
+    }
+  }
 
   return (
     <section>
@@ -28,8 +44,8 @@ const Signin = () => {
         <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
           <img src="./public/login-img.png" alt='key' className='w-full rounded-2xl ' />
         </div>
-        <div className=' w-full md:w-[67%] lg:w-[40%] lg:mt-20 mx-7'>
-          <form >
+        <div className=' w-full md:w-[67%] lg:w-[40%] lg:mt-20 mx-7 lg:ml-20'>
+          <form onSubmit={onSubmit}>
             <input className='mb-6 w-full px-4  py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type="email" id='email' value={email} onChange={onChange} placeholder='Email Address' />
 
             <div className="relative mb-6">
