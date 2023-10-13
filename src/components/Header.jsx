3ed7cb4a8400fd00699c15feb97c5ea20f4fd1aa
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-10-13 21:57:43 +0530

    complete the header component and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 const Header = () => {
   const location = useLocation()
   // console.log(location.pathname);
   const navigate = useNavigate();
+  const [pageState, setPageState] = useState("Sign In");
+  const auth = getAuth();
+  //const [] 
+
+
+
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) =>{
+      if(user){
+        setPageState("Profile");
+
+      }
+      else{
+        setPageState("Sign In")
+      }
+    })
+  }, [auth])
+
   function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
     }
   }
+
+
+
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-50 '>
       <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
@@ -19,7 +42,7 @@ const Header = () => {
           <ul className='flex space-x-10'>
             <li onClick={() => navigate("/")} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`}>Home</li>
             <li onClick={() => navigate("/offers")} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-transparent border-b-[3px] ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}>Offers</li>
-            <li onClick={() => navigate("/sign-in")} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-transparent border-b-[3px] ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`}>Sign In</li>
+            <li  onClick={() => navigate("/profile")} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-transparent border-b-[3px] ${(pathMatchRoute("/sign-in") || pathMatchRoute('/profile')) &&  "text-black border-b-red-500"}`}> {pageState}</li>
           </ul>
         </div>
       </header>
