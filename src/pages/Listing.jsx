0a96d3c442559b71cf9691c640c00cd739894eb8
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-11-02 14:02:03 +0530

    create landlord  contact component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -15,6 +15,7 @@ import SwiperCore, {
 import "swiper/css/bundle";
 
 import { getAuth } from "firebase/auth";
+import Contact from "../components/Contact";
 
 
 export default function Listing() {
@@ -75,7 +76,7 @@ export default function Listing() {
       )}
 
       <div className="flex flex-col md:flex-row max-w-6xl lg:mx-auto m-4 p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
-        <div className=" w-full h-[200px] lg-[400px] ">
+        <div className=" w-full ">
           <p className="text-2xl font-bold mb-3 text-blue-900">{listing.name} - $ {listing.offer ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
             {listing.type === "rent" ? " / month" : ""}
           </p>
@@ -99,16 +100,25 @@ export default function Listing() {
             </li>
             <li className="flex items-center whitespace-nowrap">
               <FaParking className="text-lg mr-1" />
-              {listing.parking ? "Parking Spot":"No Parking"}
+              {listing.parking ? "Parking Spot" : "No Parking"}
             </li>
             <li className="flex items-center whitespace-nowrap">
               <FaChair className="text-lg mr-1" />
-              {listing.furnished ? "Furnished":"No Furnished"}
+              {listing.furnished ? "Furnished" : "No Furnished"}
             </li>
-            
+
           </ul>
+          {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+            <div className="mt-6">
+              <button onClick={() => setContactLandlord(true)} className="px-7 py-3 bg-blue-500 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 w-full text-center duration-150 ease-in-out cursor-pointer">Contact With Landlord</button>
+            </div>
+          )}
+          {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing} />
+          )}
+
         </div>
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden "></div>
+        <div className="bg-blue-100  w-full h-[200px] lg-[400px] z-10 overflow-x-hidden "></div>
       </div>
 
     </main>
