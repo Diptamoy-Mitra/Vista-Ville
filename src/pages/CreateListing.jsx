7f7ba41882a19cd41ac0aa4599a0cfa3d8d8d5f3
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-11-04 20:05:01 +0530

    compelete home page by adding recent offers,places

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -138,10 +138,10 @@ const CreateListing = () => {
             console.log("Upload is " + progress + "% done");
             switch (snapshot.state) {
               case "paused":
-                console.log("Upload is paused");
+             //   console.log("Upload is paused");
                 break;
               case "running":
-                console.log("Upload is running");
+             //   console.log("Upload is running");
                 break;
             }
           },
--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -168,10 +168,10 @@ export default function CreateListing() {
             console.log("Upload is " + progress + "% done");
             switch (snapshot.state) {
               case "paused":
-                console.log("Upload is paused");
+              //  console.log("Upload is paused");
                 break;
               case "running":
-                console.log("Upload is running");
+               // console.log("Upload is running");
                 break;
             }
           },
